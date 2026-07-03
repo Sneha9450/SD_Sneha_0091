797244a6cc9296b32701c936717c5f850d958ea9
Sneha9450 <snehapandey9450@gmail.com>
2026-07-03 22:50:12 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# caculator
+operator= input("enter the value of operator:")
+a= int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+if(operator=='-'):
+    print(a-b)
+elif(operator=='+'):
+    print(a+b) 
+elif(operator=='*'):
+    print(a*b)
+
+    
